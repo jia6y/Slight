4c364bd0e8a2c801aca13bc01878f25716029977
NemoStone <nemo1023@gmail.com>
2016-07-06 23:03:27 +0800

    daily_update

--- lib/slight/dsl.rb
+++ lib/slight/dsl.rb
@@ -1,34 +1,34 @@
 module Slight
-	module DSL
-		def echo(str)
-			@output_buffer << str
-		end
+  module DSL
+    def echo(str)
+      @output_buffer << str
+    end
 
-		#def __dsl__transform(file_path)
-		#	cur = @output_buffer.pos
-		#	eval(IO.read(find_file), nil, find_file, __LINE__)
-		#	@output_buffer.pos = cur == 0 ? 0 : cur + 1
-		#end
+    #def __dsl__transform(file_path)
+    # cur = @output_buffer.pos
+    # eval(IO.read(find_file), nil, find_file, __LINE__)
+    # @output_buffer.pos = cur == 0 ? 0 : cur + 1
+    #end
 
-		def __dsl__define(dsl)
-			DSL.send(:define_method, dsl){|*param|
-				__dsl__packup(dsl,*param)
-			}
-		end
+    def __dsl__define(dsl)
+      DSL.send(:define_method, dsl){|*param|
+        __dsl__packup(dsl,*param)
+      }
+    end
 
-		def __dsl__packup(tag, attribute)
-			tag = tag.to_s
-			tag.sub!(/^_/,'') if tag.start_with?('_')
+    def __dsl__packup(tag, attribute)
+      tag = tag.to_s
+      tag.sub!(/^_/,'') if tag.start_with?('_')
 
-			#attribute = @filter.lookup(param)
+      #attribute = @filter.lookup(param)
 
-			echo "<#{tag}"; echo " #{attribute}" if attribute != ''; echo ">"
-			sub = yield if block_given?
-			echo sub.to_s if sub.class != Hash && sub.class != Array 
-			echo "</#{tag}>"
-		end
+      echo "<#{tag}"; echo " #{attribute}" if attribute != ''; echo ">"
+      sub = yield if block_given?
+      echo sub.to_s if sub.class != Hash && sub.class != Array 
+      echo "</#{tag}>"
+    end
 
-	end
+  end
 end
 
 
--- lib/slight/stage.rb
+++ lib/slight/stage.rb
@@ -1,35 +1,35 @@
 require 'slight/dsl'
 
 module Slight
-	class Base
-		include DSL
+  class Base
+    include DSL
 
-		def initialize(io = StringIO.new)
-			#@file_path = file_path
-			@io = io
-			@output_buffer = io
-		end
+    def initialize(io = StringIO.new)
+      #@file_path = file_path
+      @io = io
+      @output_buffer = io
+    end
 
-		def method_missing(fun, *param, &block)
-			__dsl__define(fun)
-			DSL.send(fun, *param, &block)
-		end
+    def method_missing(fun, *param, &block)
+      __dsl__define(fun)
+      DSL.send(fun, *param, &block)
+    end
 
-		private :__dsl__define, :__dsl__packup, :__dsl__transform
+    private :__dsl__define, :__dsl__packup, :__dsl__transform
 
-	end
+  end
 
-	class Compiler < Base
-		def render(src_data, local_vars, b = binding)
-			local_vars.each_pair do |key, value|
-				b.local_variable_set(key.to_sym, value)
-			end
-			cur = @output_buffer.pos
-			eval(src_data, b)
-			@output_buffer.pos = cur == 0 ? 0 : cur + 1
-		end
+  class Compiler < Base
+    def render(src_data, local_vars, b = binding)
+      local_vars.each_pair do |key, value|
+        b.local_variable_set(key.to_sym, value)
+      end
+      cur = @output_buffer.pos
+      eval(src_data, b)
+      @output_buffer.pos = cur == 0 ? 0 : cur + 1
+    end
 
-	end
+  end
 
 end
 
